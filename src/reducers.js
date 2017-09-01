d45e1895e5e70eb9674cee05b281ccf8dd1d65eb
Ryan Baxley <ryan.baxley1@gmail.com>
2017-08-31 21:10:13 -0400

    Removing unused reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 
-import map from './Map/MapReducer';
 import rainData from './RainData/RainDataReducer';
 
 const weatherApp = combineReducers({
-  map,
   rainData,
 });
 
